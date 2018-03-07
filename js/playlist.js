fa3b35745ae564a372d241c7ee6c1cbdf869a8e8
Christy <caburris@ucdavis.edu>
2018-03-07 14:47:57 -0800

    using for loop with arrays

--- js/playlist.js
+++ js/playlist.js
@@ -1,4 +1,34 @@
-var playList= [ ];
+var playList= [
+    'I Did It My Way',
+     'Respect',
+     'Imagine',
+     'Born to Run',
+     'Louie Louie',
+     'Maybellene'
+ ];
+
+ function print(message) {
+   document.write(message);
+ }
+
+function printList( list) {
+    var listHTML = '<ol>';
+    for ( var i = 0; i < list.length; i += 1 ) {
+        listHTML += '<li>' + list[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    print(listHTML);
+}
+printList(playList);
+
+
+
+
+
+
+
+/*---Notes---*/
+/*
 playList.push('I did It My Way'); //adds to the end of the array
 playList.push('Respect', 'Imagine');
 playList.unshift('Born to Run'); //adds to the beginning of the array
@@ -8,4 +38,6 @@ printList ( playList );
 
 playList.pop();  //removes and returns last item. Can use it to retrieve last item
 
-playList.shift(); //removes item from beginning
+playList.shift(); //removes and returns item from beginning
+
+*/
