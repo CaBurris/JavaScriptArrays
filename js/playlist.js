6172cf3b8bc07b111b6ec2102bfc211a6f754a2d
Christy <caburris@ucdavis.edu>
2018-03-07 14:26:40 -0800

    learn how to add and remove items in array

--- js/playlist.js
+++ js/playlist.js
@@ -1,6 +1,11 @@
 var playList= [ ];
-playList.push('I did It My Way');
+playList.push('I did It My Way'); //adds to the end of the array
 playList.push('Respect', 'Imagine');
-playList.unshift('Born to Run');
+playList.unshift('Born to Run'); //adds to the beginning of the array
 playList.unshift('Louie Louie', 'Maybellene' );
 printList ( playList );
+
+
+playList.pop();  //removes and returns last item. Can use it to retrieve last item
+
+playList.shift(); //removes item from beginning
